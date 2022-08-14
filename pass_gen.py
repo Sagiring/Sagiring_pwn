5c452b1cd5665e3403414d7fff21d18c17c6ccf8
Sagiring <752767115@qq.com>
2022-08-15 00:02:09 +0800

    已简化判断 修改为二进制条件判断

--- pass_gen.py
+++ pass_gen.py
@@ -5,44 +5,53 @@ while 1:
     # 判断合理性
     
     
-
-    have_upper = False
-    have_lower = False
-    have_digit = False
-    have_puctuation = False
-    have_len= len(user_password) >= 8
+    password_state=0b00000
+    # have_upper = False
+    # have_lower = False
+    # have_digit = False
+    # have_puctuation = False
+    
 
     for char in user_password:
         if char in string.ascii_uppercase:
-            have_upper = True
+            password_state |=  0b10000
+             # have_upper = True
         elif char in string.ascii_lowercase:
-            have_lower = True
+            password_state |=  0b01000
+            # have_lower = True
         elif char in string.digits:
-            have_digit = True
+            password_state |=  0b00100
+            # have_digit = True
         else:
-            have_puctuation = True
+            password_state |=  0b00010
+            # have_puctuation = True
+
+    if len(user_password) >= 8:
+        password_state |=  0b00001
+        
 
     # 输出
-    is_secure = (have_upper
-                and have_len 
-                and have_lower 
-                and have_digit 
-                and have_puctuation)
+    # is_secure = (have_upper
+    #             and have_len 
+    #             and have_lower 
+    #             and have_digit 
+    #             and have_puctuation)
+    # is_secure = (password_state == 0b11111) 
     
-    if is_secure:
+    if password_state == 0b11111:
         print("Password is in rule")
         break
     else:
         prompt="Password is not in rule,"
-        if not have_len:
+        if not password_state & 0b00001 == 0b00001:
             prompt+='len is not enough,'
-        if not have_upper:
+        if not password_state & 0b10000 == 0b10000:
             prompt+='no upper char,'
-        if not have_lower:
+        if not password_state & 0b01000 == 0b01000:
             prompt+='no lower char,'
-        if not have_digit:
+        if not password_state & 0b00100 == 0b00100:
             prompt+='no digit,'
-        if not have_puctuation:
+        if not password_state & 0b00010 == 0b00010:
             prompt+='no puctuation,'
         prompt=prompt[:-1]
         prompt+='.'
