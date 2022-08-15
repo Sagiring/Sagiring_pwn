5073e509b12b5206d4e8fc12f07355fb0d3c49f4
Sagiring <752767115@qq.com>
2022-08-15 09:38:15 +0800

    函数封装

--- pass_gen.py
+++ pass_gen.py
@@ -1,59 +1,58 @@
 import string
-while 1:
-    user_password= input("请输入:")
 
-    # 判断合理性
-    
-    
-    password_state=0b00000
-    # have_upper = False
-    # have_lower = False
-    # have_digit = False
-    # have_puctuation = False
-    
+def evaluate_password(password):
+        result=False
+        password_state=0b00000
 
-    for char in user_password:
-        if char in string.ascii_uppercase:
-            password_state |=  0b10000
-             # have_upper = True
-        elif char in string.ascii_lowercase:
-            password_state |=  0b01000
-            # have_lower = True
-        elif char in string.digits:
-            password_state |=  0b00100
-            # have_digit = True
+        for char in password:
+            if char in string.ascii_uppercase:
+                password_state |=  0b10000
+    
+            elif char in string.ascii_lowercase:
+                password_state |=  0b01000
+
+            elif char in string.digits:
+                password_state |=  0b00100
+    
+            else:
+                password_state |=  0b00010
+        if len(password) >= 8:
+            password_state |=  0b00001
+            
+
+        if password_state == 0b11111:
+            print("Password is in rule")
+            result=True
         else:
-            password_state |=  0b00010
-            # have_puctuation = True
+            prompt="Password is not in rule,"
+            if  password_state & 0b00001 == 0:
+                prompt+='len is not enough,'
+            if  password_state & 0b10000 == 0:
+                prompt+='no upper char,'
+            if  password_state & 0b01000 == 0:
+                prompt+='no lower char,'
+            if  password_state & 0b00100 == 0:
+                prompt+='no digit,'
+            if  password_state & 0b00010 == 0:
+                prompt+='no puctuation,'
+            prompt=prompt[:-1]
+            prompt+='.'
+            print(prompt)
 
-    if len(user_password) >= 8:
-        password_state |=  0b00001
-        
+        return result
 
-    # 输出
-    # is_secure = (have_upper
-    #             and have_len 
-    #             and have_lower 
-    #             and have_digit 
-    #             and have_puctuation)
-    # is_secure = (password_state == 0b11111) 
-    
-    if password_state == 0b11111:
-        print("Password is in rule")
-        break
-    else:
-        prompt="Password is not in rule,"
-        if  password_state & 0b00001 == 0:
-            prompt+='len is not enough,'
-        if  password_state & 0b10000 == 0:
-            prompt+='no upper char,'
-        if  password_state & 0b01000 == 0:
-            prompt+='no lower char,'
-        if  password_state & 0b00100 == 0:
-            prompt+='no digit,'
-        if  password_state & 0b00010 == 0:
-            prompt+='no puctuation,'
-        prompt=prompt[:-1]
-        prompt+='.'
-        print(prompt)
-#123
+
+def main():
+    while 1:
+        # 用户输入密码
+        user_password= input("请输入:")
+        # 判断合理性
+        if evaluate_password(user_password):
+            break
+
+main()
+
+
+
+
+
