fc57fff47ff41d36a5df6babe4d2d9426cbf436d
Sagiring <752767115@qq.com>
2022-09-01 10:07:09 +0800

    bot能跑起来啦！！！~~~

--- Py/OCR_bot/bilibili_c_immedietly.py
+++ Py/OCR_bot/bilibili_c_immedietly.py
@@ -11,54 +11,54 @@ def get_bilibili_hot():
         # headers["User-Agent"] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 
         cookies = {
-            'buvid3': '19F99527-07B1-434F-852D-AD240DA95712148831infoc',
-            'LIVE_BUVID': 'AUTO4716261050593882',
-            'rpdid': '|(J|YmYulk~~0J\'uYkYuJJm|u',
-            'video_page_version': 'v_old_home',
-            'fingerprint_s': '9b0bccf4ec57734720e42828cf636185',
-            'i-wanna-go-back': '-1',
-            'buvid_fp_plain': 'undefined',
-            'buvid4': 'FA94223F-82CA-ACC9-D0BB-E38B02B4731991204-022012417-4HxpIjLfbKYv65KDSYmcdw%3D%3D',
-            'fingerprint3': '0111e39952fcd2d0e77176058d70888b',
-            'CURRENT_BLACKGAP': '0',
-            'hit-dyn-v2': '1',
-            'nostalgia_conf': '-1',
-            'CURRENT_QUALITY': '116',
-            'blackside_state': '0',
-            '_uuid': 'A99993BA-C72A-4106B-8344-C9D7109BCAFBF01405infoc',
-            'fingerprint': '0b5f8746827d7037afa0e55420fc4564',
-            'buvid_fp': '0b5f8746827d7037afa0e55420fc4564',
-            'DedeUserID': '23111878',
-            'DedeUserID__ckMd5': '0d9aed6047896e63',
-            'b_ut': '5',
-            'bp_article_offset_23111878': '689267583018336400',
-            'SESSDATA': '87cc0878%2C1677249680%2C08686%2A81',
-            'bili_jct': '8de252bd1bd48acdcb4a14bd32bd2b32',
-            'sid': '6ofrn8b3',
-            'CURRENT_FNVAL': '4048',
-            'innersign': '0',
-            'b_lsid': '265A5528_182EA0BF7C9',
-            'PVID': '4',
-            'bp_video_offset_23111878': '699843450464370700',
-        }
+        'buvid3': '19F99527-07B1-434F-852D-AD240DA95712148831infoc',
+        'LIVE_BUVID': 'AUTO4716261050593882',
+        'rpdid': '|(J|YmYulk~~0J\'uYkYuJJm|u',
+        'video_page_version': 'v_old_home',
+        'fingerprint_s': '9b0bccf4ec57734720e42828cf636185',
+        'i-wanna-go-back': '-1',
+        'buvid_fp_plain': 'undefined',
+        'buvid4': 'FA94223F-82CA-ACC9-D0BB-E38B02B4731991204-022012417-4HxpIjLfbKYv65KDSYmcdw%3D%3D',
+        'fingerprint3': '0111e39952fcd2d0e77176058d70888b',
+        'CURRENT_BLACKGAP': '0',
+        'hit-dyn-v2': '1',
+        'nostalgia_conf': '-1',
+        'CURRENT_QUALITY': '116',
+        'blackside_state': '0',
+        '_uuid': 'A99993BA-C72A-4106B-8344-C9D7109BCAFBF01405infoc',
+        'fingerprint': '0b5f8746827d7037afa0e55420fc4564',
+        'buvid_fp': '0b5f8746827d7037afa0e55420fc4564',
+        'DedeUserID': '23111878',
+        'DedeUserID__ckMd5': '0d9aed6047896e63',
+        'b_ut': '5',
+        'CURRENT_FNVAL': '4048',
+        'innersign': '0',
+        'SESSDATA': '92089377%2C1677495170%2Cf6a2f%2A81',
+        'bili_jct': 'e1f20d7b43ef9b44fc321addf4ed0b3f',
+        'sid': 'parqjy1w',
+        'b_lsid': 'DB9D9F6F_182F3DB085D',
+        'bp_video_offset_23111878': '700562458064978000',
+        'PVID': '3',
+    }
 
         headers = {
             'authority': 'api.bilibili.com',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept': '*/*',
             'accept-language': 'zh-CN,zh;q=0.9',
-            'cache-control': 'max-age=0',
             # Requests sorts cookies= alphabetically
-            # 'cookie': 'buvid3=19F99527-07B1-434F-852D-AD240DA95712148831infoc; LIVE_BUVID=AUTO4716261050593882; rpdid=|(J|YmYulk~~0J\'uYkYuJJm|u; video_page_version=v_old_home; fingerprint_s=9b0bccf4ec57734720e42828cf636185; i-wanna-go-back=-1; buvid_fp_plain=undefined; buvid4=FA94223F-82CA-ACC9-D0BB-E38B02B4731991204-022012417-4HxpIjLfbKYv65KDSYmcdw%3D%3D; fingerprint3=0111e39952fcd2d0e77176058d70888b; CURRENT_BLACKGAP=0; hit-dyn-v2=1; nostalgia_conf=-1; CURRENT_QUALITY=116; blackside_state=0; _uuid=A99993BA-C72A-4106B-8344-C9D7109BCAFBF01405infoc; fingerprint=0b5f8746827d7037afa0e55420fc4564; buvid_fp=0b5f8746827d7037afa0e55420fc4564; DedeUserID=23111878; DedeUserID__ckMd5=0d9aed6047896e63; b_ut=5; bp_article_offset_23111878=689267583018336400; SESSDATA=87cc0878%2C1677249680%2C08686%2A81; bili_jct=8de252bd1bd48acdcb4a14bd32bd2b32; sid=6ofrn8b3; CURRENT_FNVAL=4048; innersign=0; b_lsid=265A5528_182EA0BF7C9; PVID=4; bp_video_offset_23111878=699843450464370700',
+            # 'cookie': 'buvid3=19F99527-07B1-434F-852D-AD240DA95712148831infoc; LIVE_BUVID=AUTO4716261050593882; rpdid=|(J|YmYulk~~0J\'uYkYuJJm|u; video_page_version=v_old_home; fingerprint_s=9b0bccf4ec57734720e42828cf636185; i-wanna-go-back=-1; buvid_fp_plain=undefined; buvid4=FA94223F-82CA-ACC9-D0BB-E38B02B4731991204-022012417-4HxpIjLfbKYv65KDSYmcdw%3D%3D; fingerprint3=0111e39952fcd2d0e77176058d70888b; CURRENT_BLACKGAP=0; hit-dyn-v2=1; nostalgia_conf=-1; CURRENT_QUALITY=116; blackside_state=0; _uuid=A99993BA-C72A-4106B-8344-C9D7109BCAFBF01405infoc; fingerprint=0b5f8746827d7037afa0e55420fc4564; buvid_fp=0b5f8746827d7037afa0e55420fc4564; DedeUserID=23111878; DedeUserID__ckMd5=0d9aed6047896e63; b_ut=5; CURRENT_FNVAL=4048; innersign=0; SESSDATA=92089377%2C1677495170%2Cf6a2f%2A81; bili_jct=e1f20d7b43ef9b44fc321addf4ed0b3f; sid=parqjy1w; b_lsid=DB9D9F6F_182F3DB085D; bp_video_offset_23111878=700562458064978000; PVID=3',
+            'origin': 'https://www.bilibili.com',
+            'referer': 'https://www.bilibili.com/',
             'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
         }
+
+
 
         params = {
             'ps': '20',
@@ -75,7 +75,7 @@ def get_bilibili_hot():
         page +=1
         if page > 1:
             break
-    # print(bilibili_data[0])
+    # print(bilibili_data)
     return bilibili_data
 
     
--- Py/OCR_bot/wechat_bot.py
+++ Py/OCR_bot/wechat_bot.py
@@ -1,11 +1,14 @@
 from PIL import ImageGrab
 import time
 import win32gui
-import win32con
+# import win32con
 import pyperclip
 import pyautogui
 from aip import AipOcr
 import bilibili_c_immedietly
+import hashlib
+import base64
+import hashlib
 
 BAIDU_OCR_APP_ID = '27238006'
 BAIDU_OCR_API_KEY = 'ksVOt8Rw4CN3yrEEGpi0nOVx'
@@ -23,11 +26,11 @@ def getwindow_position():
 
     # 定位到窗体
     # 置顶窗口
-    if win32gui.IsIconic(window_hwnd):
-        win32gui.ShowWindow(WINDOW_TITLE,win32con.SW_SHOW)
-        # 最小化顶置有问题
-    else:
-        win32gui.SetForegroundWindow(window_hwnd)
+    # if win32gui.IsIconic(window_hwnd):
+    #     win32gui.ShowWindow(WINDOW_TITLE,win32con.SW_SHOW)
+    #     # 最小化顶置有问题
+    # else:
+    win32gui.SetForegroundWindow(window_hwnd)
     # win32gui.SetFocus(window_hwnd)
 
     window_position = win32gui.GetWindowRect(window_hwnd)
@@ -36,21 +39,27 @@ def getwindow_position():
     
     return (window_position)
 
-def window_caputure(window_position):
+def window_caputure_isnewmsg(window_position):
     # print(window_position) 
+    is_new_msg = 0
 
-    wechat_begin_Px = window_position[0] * 1.5 + 450
-    wechatd_begin_Py = window_position[1]* 1.5 + 420
+    wechat_begin_Px = window_position[0] * 1.5 + 460
+    wechatd_begin_Py = window_position[1]* 1.5 + 450
 	#话泡最长度底部像素
-    wechat_End_Px = wechat_begin_Px + 1340 - 650
-    wechat_End_Py = wechatd_begin_Py + 100
+    wechat_End_Px = wechat_begin_Px + 1340 - 660
+    wechat_End_Py = wechatd_begin_Py + 100 - 30
 	# 截图保存,输入屏幕左上角和右下角的坐标
     # 450, 0, 1340,750
     pic = ImageGrab.grab(bbox=(wechat_begin_Px, wechatd_begin_Py, wechat_End_Px, wechat_End_Py))
 # print(time.time())
     pic_path = f'E:\Study\Py\OCR_bot\pic\pic.jpg'
     pic.save(pic_path)
-
+    pic_hash_path = f'E:\Study\Py\OCR_bot\pic\pic_hash.jpg'
+    
+    if pic_md5(pic_path)!=pic_md5(pic_hash_path):
+        is_new_msg = 1
+        pic.save(pic_hash_path)
+    return is_new_msg
     # 窗体标题  用于定位窗体
 def get_file_content(filePath):
 	    with open(filePath, 'rb') as fp:
@@ -70,27 +79,48 @@ def send_msg(window_position, res):
     pyautogui.hotkey('ctrl','v')
     pyautogui.click(window_position[0] + 1200 ,window_position[1] + 720)
 
+def msg_creat(window_position):
+    ocr_data = ocr_data_get(BAIDU_OCR_APP_ID, BAIDU_OCR_API_KEY, BAIDU_OCR_SECRET_KEY)
+    # print(ocr_data)
+    if ocr_data['words_result']:
+        news_msg = ocr_data['words_result'][len(ocr_data['words_result'])-1]['words']
+        print(news_msg)
+    
+        if news_msg == 'b站热搜':
+            res = f'top1《{bilibili_c_immedietly.get_bilibili_hot()[0]}》\ntop2《{bilibili_c_immedietly.get_bilibili_hot()[1]}》\ntop3《{bilibili_c_immedietly.get_bilibili_hot()[2]}》\n'
+            send_msg(window_position, res)
+        elif news_msg == '晚安':
+            res = '好梦捏！'
+            send_msg(window_position, res)
+        else:
+            res = ''
+    else:
+        print('Nothing found')
+    # print(res)
+
+def pic_md5(pic_path):
+    with open(pic_path,'rb') as pic_hash:
+            pic_data = pic_hash.read()
+            base64_data = base64.b64encode(pic_data)
+        # print(base64_data)
+            data = hashlib.md5(base64_data).hexdigest()
+            return data
+
 
 
 def main():
- 
     
-    window_position = getwindow_position()
-    window_caputure(window_position)
-    ocr_data = ocr_data_get(BAIDU_OCR_APP_ID, BAIDU_OCR_API_KEY, BAIDU_OCR_SECRET_KEY)
-    # print(ocr_data)
-    news_msg = ocr_data['words_result'][len(ocr_data['words_result'])-1]['words']
-    print(news_msg)
-    if news_msg == 'b站热搜':
-        res = f'top1《{bilibili_c_immedietly.get_bilibili_hot()[0]}》\ntop2《{bilibili_c_immedietly.get_bilibili_hot()[1]}》\ntop3《{bilibili_c_immedietly.get_bilibili_hot()[2]}》\n'
-        send_msg(window_position, res)
-    elif news_msg == '晚安':
-        res = '好梦捏！'
-        send_msg(window_position, res)
-    else:
-        res = ''
-    print(res)
-    
-
+    while 1:
+        time.sleep(3)
+        window_position = getwindow_position()
+        is_new_msg = window_caputure_isnewmsg(window_position)
+        print(is_new_msg)
+        
+        
+        if is_new_msg:
+            is_new_msg = 0
+            msg_creat(window_position)
+            
 
 main()
+
