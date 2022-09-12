b8b32d8a245c284bb4c27dfa0ea2bf2a091c4e6d
Sagiring <752767115@qq.com>
2022-09-12 16:11:38 +0800

    Clock Java

---
+++ Java_small_program/Clock/clock.java
@@ -0,0 +1,21 @@
+package Clock;
+
+public class clock {
+    private Display hour = new Display(24);
+    private Display minute = new Display(60);
+
+    public void start() {
+        while (true) {
+            minute.increase();
+            if (minute.get_value() == 0) {
+                hour.increase();
+            }
+            System.out.printf("%02d:%02d\n", hour.get_value(), minute.get_value());
+        }
+    }
+
+    public static void main(String[] args) {
+        clock Clock = new clock();
+        Clock.start();
+    }
+}
--- Java_small_program/Main.java
+++ Java_small_program/Main.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int money_amount = 0;
-        Scanner in = new Scanner(System.in);
-        System.out.print("请投币>");
-        money_amount = in.nextInt();
-        if (money_amount >= 10) {
-            System.out.println("找零>" + (money_amount - 10));
-        } else {
-            System.out.println("这些钱无法购买一张票");
-        }
-        in.close();
+        fuction();
+    }
+
+    private static void fuction() {
+        String test_string = "123";
+        System.out.println((test_string.charAt(2)));
     }
 }
