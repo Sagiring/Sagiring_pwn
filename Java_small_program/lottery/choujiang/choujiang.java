d24b1049b1d3b46a05ef3e27af01fa94573a5b67
Sagiring <752767115@qq.com>
2022-11-06 11:12:59 +0800

    java_file_correct_change

---
+++ Java_small_program/lottery/choujiang/choujiang.java
@@ -0,0 +1,14 @@
+package choujiang;
+
+
+
+public class choujiang {
+    public String lottery_1(){
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java_small_program/python_tuple/py_tuple/python_tuple.java
@@ -0,0 +1,13 @@
+package py_tuple;
+
+import java.util.HashSet;
+
+public class python_tuple {
+    public static void main(String[] args) {
+        HashSet<String> s = new HashSet<String>();
+        s.add("first");
+        s.add("second");
+        System.out.println(s);
+        //不能重复的元组
+    }
+}
