7fd6ba1e32a54dfd5e24d43f4bfc5d9f87fefcdf
Sagiring <752767115@qq.com>
2023-01-11 12:16:53 +0800

    name change

--- Java/Java_small_program/homework_11_6/high_data/height.java
+++
@@ -1,4 +0,0 @@
-package high_data;
-public interface height {
- public double getdata();
-}
--- Python/Py_small_program/试试/test.py
+++
@@ -1,4 +0,0 @@
-x = 6
-result = (x ** 6) + (2* x ** 5) + (2 * x ** 3) + (15 * x ** 2) - (3 * x)
-print(result)
-print(result%7)
